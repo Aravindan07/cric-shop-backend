96a25b9af8a34c72741d294ed4827e005e3d3537
Aravindan <aravindanravi33@gmail.com>
2021-05-10 18:40:57 +0530

    fix: fixed a mistake in route

--- routes/quantity.router.js
+++ routes/quantity.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { updateQuantityInCart } = require("../controllers/quantity.controller");
 
-router.route("").put(updateQuantityInCart);
+router.route("/").put(updateQuantityInCart);
 
 module.exports = router;
