4af8281edc5fe215ffb67c71da4f12f47228b5b4
Aravindan <aravindanravi33@gmail.com>
2021-05-10 13:21:08 +0530

    add: added default value for qunatity

--- model/cart.model.js
+++ model/cart.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const cartListSchema = new Schema({
 	product: { type: Schema.Types.ObjectId, ref: "Product" },
-	quantity: { type: Number, default: 0 },
+	quantity: { type: Number, default: 1 },
 });
 
 module.exports = mongoose.model("Cartlist", cartListSchema);
